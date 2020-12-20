d0bb34e8d4b346f9b257c5b5257f6a4bd3facda0
Shashwat-Joshi <shashwat2002joshi@gmail.com>
2020-12-20 17:31:44 +0530

    Day 2

---
+++ Day2/lineClipperWidget/android/app/src/main/kotlin/com/example/clipperWidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clipperWidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Day2/lineClipperWidget/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Clipper'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ClipPath(
+          clipper: new MyClipper(),
+          child: Image(
+            image: AssetImage('images/image.jpg'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = new Path();
+    path.moveTo(size.width, size.height);
+    path.lineTo(size.width / 2, size.height / 2);
+    path.lineTo(0.0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
